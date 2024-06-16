d7d4857db67a45d545d1c7266d2ba655c5b01423
RonnieTz <ronis.tzol@gmail.com>
2024-06-16 14:38:36 +0100

    insert labels in beam images

--- app/components/RafterView.tsx
+++ app/components/RafterView.tsx
@@ -1,10 +1,24 @@
 const RafterView = () => {
   return (
-    <div className="rafter-side-view-container">
+    <div className="rafter-view-container">
       <figure>
         <figcaption>Choose the rafter's angle shown in the scope.</figcaption>
-        <img src="/rafter_scope_side.png" alt="spectator view" />
-        <img src="/top_view.png" alt="spectator view" />
+        <div className="container">
+          <div className="label r">r</div>
+          <img src="/rafter_scope_side.png" alt="spectator view" />
+        </div>
+        <div className="container">
+          <div className="label t">t</div>
+          <img src="/top_view.png" alt="spectator view" />
+        </div>
+        <div className="container">
+          <div className="label w">w</div>
+          <img src="/rafter_scope_top.png" alt="spectator view" />
+        </div>
+        <div className="container">
+          <div className="label h">h</div>
+          <img src="/rafter_scope_close.png" alt="spectator view" />
+        </div>
       </figure>
     </div>
   );

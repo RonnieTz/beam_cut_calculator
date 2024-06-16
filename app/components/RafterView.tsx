6025e06e6172b306ebf6f16b6252b9c7808fea59
RonnieTz <ronis.tzol@gmail.com>
2024-06-16 14:14:03 +0100

    first commit

---
+++ app/components/RafterView.tsx
@@ -0,0 +1,12 @@
+const RafterView = () => {
+  return (
+    <div className="rafter-side-view-container">
+      <figure>
+        <figcaption>Choose the rafter's angle shown in the scope.</figcaption>
+        <img src="/rafter_scope_side.png" alt="spectator view" />
+        <img src="/top_view.png" alt="spectator view" />
+      </figure>
+    </div>
+  );
+};
+export default RafterView;
---
+++ app/components/SpactatorView.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+const SpactatorView = () => {
+  return (
+    <div className="spactator-view-container">
+      <figure>
+        <figcaption>Calculate the cut angles for the rafter beam.</figcaption>
+        <img src="/spectator_view.png" alt="spectator view" />
+      </figure>
+    </div>
+  );
+};
+export default SpactatorView;

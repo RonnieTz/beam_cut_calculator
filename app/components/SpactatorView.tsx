ffdb6e80bb75594102ff61efb4167a6b30f129c6
RonnieTz <ronis.tzol@gmail.com>
2024-06-20 17:33:02 +0100

    small change in text

--- app/components/SpactatorView.tsx
+++ app/components/SpactatorView.tsx
@@ -2,7 +2,9 @@ const SpactatorView = () => {
   return (
     <div className="spactator-view-container">
       <figure>
-        <figcaption>Calculate the cut angles for the rafter beam.</figcaption>
+        <figcaption>
+          Calculate the cut angles for the valley rafter beam.
+        </figcaption>
         <img src="/spectator_view.png" alt="spectator view" />
       </figure>
     </div>
